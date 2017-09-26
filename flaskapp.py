92963428a67916662cd389e7ce4b8bc7166b1701
Jan Cerny <jan.cerny@deliveroo.co.uk>
2017-09-26 16:57:59 +0100

    postgres test

--- flaskapp.py
+++ flaskapp.py
@@ -1,12 +1,28 @@
 import os
 from flask import Flask
+import psycopg2
+import urllib.parse as urlparse
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db_url = urlparse.urlparse(os.environ['DATABASE_URL'])
+conn = psycopg2.connect(
+    database=db_url.path[1:],
+    user=db_url.username,
+    password=db_url.password,
+    host=db_url.hostname,
+    port=db_url.port
+)
+cur = conn.cursor()
 
 
 @app.route('/')
 def index():
-    return 'running'
+    cur.execute("SELECT * FROM test;")
+    result = cur.fetchone()
+    return result[1]
 
 
 if __name__ == '__main__':
